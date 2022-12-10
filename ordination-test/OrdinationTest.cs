dec0016d65167ac200d17c770dd7daa4909003ec
Cecilie Bossov Westerdahl <eaacbw@students.eaaa.dk>
2022-12-10 18:27:33 +0100

    Tilføjelse af kommentar ved tests

--- ordination-test/OrdinationTest.cs
+++ ordination-test/OrdinationTest.cs
@@ -10,21 +10,21 @@ public class OrdinationTest
     public void AntalDageTest()
     {
         // Gyldig data
-        // TC1 - KortOrdinationsPeriode
+        // TC1: KortOrdinationsPeriode
         PN tc1 = new PN(new DateTime(2023, 01, 01), new DateTime(2023, 01, 08), 123, new Laegemiddel("Paracetamol", 1, 1.5, 2, "Ml"));
 
         int antalDage_tc1 = tc1.antalDage();
 
         Assert.AreEqual(7, antalDage_tc1);
 
-        // TC2 - MellemlangOrdinationsPeriode
+        // TC2: MellemlangOrdinationsPeriode
         PN tc2 = new PN(new DateTime(2023, 01, 01), new DateTime(2023, 02, 01), 123, new Laegemiddel("Paracetamol", 1, 1.5, 2, "Ml"));
 
         int antalDage_tc2 = tc2.antalDage();
 
         Assert.AreEqual(31, antalDage_tc2);
 
-        // TC3 - langOrdinationsPeriode
+        // TC3: langOrdinationsPeriode
         PN tc3 = new PN(new DateTime(2023, 01, 01), new DateTime(2024, 01, 01), 123, new Laegemiddel("Paracetamol", 1, 1.5, 2, "Ml"));
 
         int antalDage_tc3 = tc3.antalDage();
@@ -39,16 +39,16 @@ public class OrdinationTest
     {
 
         // Ugyldig data - slutdato en dag før startdato
-        // TC4:
+        // TC4: SlutDato1DagFørStartDato
         PN tc4 = new PN(new DateTime(2023, 01, 01), new DateTime(2022, 12, 31), 123, new Laegemiddel("Paracetamol", 1, 1.5, 2, "Ml"));
 
         int antalDage_tc4 = tc4.antalDage();
 
         Assert.AreEqual(1, antalDage_tc4);
-        
+
 
         // Ugyldig data - slutdato 14 dage før startdato
-        // TC5:
+        // TC5: SlutDato14DageFørStartDato
         PN tc5 = new PN(new DateTime(2023, 01, 01), new DateTime(2022, 12, 18), 123, new Laegemiddel("Paracetamol", 1, 1.5, 2, "Ml"));
 
         int antalDage_tc5 = tc5.antalDage();
--- ordination-test/PNTest.cs
+++ ordination-test/PNTest.cs
@@ -13,6 +13,7 @@ public class PNTest
 
 
         // TC1 - true hvis ordinationen gives inden for ordinationens gyldighedsperiode.
+        // TC1: TestGivesDenMellemStartOgSlut
         PN tc1 = new PN(new DateTime(2023, 01, 01), new DateTime(2023, 01, 08), 123, new Laegemiddel("Fucidin", 0.025, 0.025, 0.025, "Styk"));
 
         bool givDosis_tc1 = tc1.givDosis(new Dato { dato = new DateTime(2023, 01, 05).Date });
@@ -20,16 +21,18 @@ public class PNTest
         Assert.AreEqual(true, givDosis_tc1);
 
         // TC2 - true hvis ordinationen gives inden for ordinationens gyldighedsperiode.
+        // TC2: TestGivesDenPåStart
         PN tc2 = new PN(new DateTime(2023, 01, 01), new DateTime(2023, 02, 01), 123, new Laegemiddel("Fucidin", 0.025, 0.025, 0.025, "Styk"));
 
-        bool givDosis_tc2 = tc2.givDosis(new Dato { dato = new DateTime(2023, 01, 04).Date });
+        bool givDosis_tc2 = tc2.givDosis(new Dato { dato = new DateTime(2023, 01, 01).Date });
 
         Assert.AreEqual(true, givDosis_tc2);
 
         // TC3 - true hvis ordinationen gives inden for ordinationens gyldighedsperiode.
+        // TC3: TestGivesDenPåSlut
         PN tc3 = new PN(new DateTime(2023, 01, 01), new DateTime(2024, 01, 01), 123, new Laegemiddel("Fucidin", 0.025, 0.025, 0.025, "Styk"));
 
-        bool givDosis_tc3 = tc3.givDosis(new Dato { dato = new DateTime(2023, 1, 12).Date });
+        bool givDosis_tc3 = tc3.givDosis(new Dato { dato = new DateTime(2024, 01, 01).Date });
 
         Assert.AreEqual(true, givDosis_tc3);
 
@@ -37,13 +40,15 @@ public class PNTest
         // Ugyldig data
 
         // TC4 - false hvis ordinationen gives uden for ordinationens gyldighedsperiode
+        // TC4: TestGivesDenFørStart
         PN tc4 = new PN(new DateTime(2023, 01, 01), new DateTime(2023, 01, 12), 123, new Laegemiddel("Fucidin", 0.025, 0.025, 0.025, "Styk"));
 
-        bool givDosis_tc4 = tc3.givDosis(new Dato { dato = new DateTime(2024, 12, 31).Date });
+        bool givDosis_tc4 = tc3.givDosis(new Dato { dato = new DateTime(2022, 12, 31).Date });
 
         Assert.AreEqual(false, givDosis_tc4);
 
         // TC5 - false hvis ordinationen gives uden for ordinationens gyldighedsperiode
+        // TC5: TestGivesDenEfterSlut
         PN tc5 = new PN(new DateTime(2023, 01, 1), new DateTime(2023, 01, 12), 123, new Laegemiddel("Fucidin", 0.025, 0.025, 0.025, "Styk"));
 
         bool givDosis_tc5 = tc5.givDosis(new Dato { dato = new DateTime(2023, 01, 13).Date });
